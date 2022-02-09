f0fb18fe7b1b41b93118f9434fbece2dbb24608a
Gabriel Hazi <hazigabriel@gmail.com>
2022-02-09 02:46:05 +0200

    added landing page along with most of its styling

---
+++ src/components/aboutPage/aboutPage.js
@@ -0,0 +1,23 @@
+function AboutPage() {
+
+    return (
+        <section className="sectionWrapper" id="about">
+            <div className="sectionContent">
+                <div className="leftColumn">
+                    <p>
+                        Self-taught front end developer located in Bucharest with a strong interest in learning how to write great code
+                    </p>
+                    <p>
+                        In the near future I’m looking forward to learn Redux, Typescript and Node.js. In the distant future I plan to learn about the back-end and become a Full-stack developer. 
+                    </p>
+                    <p>
+                        Besides coding and adding pictures with myself and cool background rocks (pretty cool rocks, right?) I enjoy cycling, cooking, taking pictures and reading. 
+                    </p>
+
+                </div>
+                <div className="rightColumn"></div>
+            </div>
+        </section>
+    )
+}
+export default AboutPage
